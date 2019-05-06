b018207c83ae2c5fa7a802148feb817d58da977e
PetejStone <petejstoney@gmail.com>
2019-05-06 14:07:56 -0700

    added Link components

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -22,11 +23,19 @@ export default class MovieList extends Component {
 
   render() {
     return (
+      
+      
       <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+      
+      {console.log(this.state.movies)}
+      
+        {this.state.movies.map((movie) => (
+          <Link to={`/movies/${movie.id}`}>
+            <MovieDetails key={movie.id} movie={movie} />
+          </Link>
         ))}
       </div>
+      
     );
   }
 }
